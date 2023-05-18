43c1e9fdd0c75bcb01cdb8737b26f6710d9b9671
Jake Jacobo <jakejacobo2015@gmail.com>
2023-05-18 22:27:35 +0800

    Add project files.

---
+++ fileio/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btn_load = New System.Windows.Forms.Button()
+        Me.txtbox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btn_load
+        '
+        Me.btn_load.Location = New System.Drawing.Point(271, 229)
+        Me.btn_load.Name = "btn_load"
+        Me.btn_load.Size = New System.Drawing.Size(162, 53)
+        Me.btn_load.TabIndex = 0
+        Me.btn_load.Text = "LOAD"
+        Me.btn_load.UseVisualStyleBackColor = True
+        '
+        'txtbox1
+        '
+        Me.txtbox1.Location = New System.Drawing.Point(95, 12)
+        Me.txtbox1.Multiline = True
+        Me.txtbox1.Name = "txtbox1"
+        Me.txtbox1.Size = New System.Drawing.Size(521, 198)
+        Me.txtbox1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtbox1)
+        Me.Controls.Add(Me.btn_load)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_load As Button
+    Friend WithEvents txtbox1 As TextBox
+End Class
---
+++ fileio/Form1.vb
@@ -0,0 +1,122 @@
+﻿Imports System.Globalization
+Imports System.IO
+Imports System.Windows.Forms.VisualStyles
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub btn_load_Click(sender As Object, e As EventArgs) Handles btn_load.Click
+
+        Dim FileDir = ""
+        Dim openFileDilog As OpenFileDialog = New OpenFileDialog()
+        Dim palinString As String
+        Dim tempStr As String = ""
+        Dim stringReader As String
+        openFileDilog.Title = "Browse Text File"
+        openFileDilog.InitialDirectory = "C:\\"
+        openFileDilog.Filter = "Text Files|*.txt"
+        openFileDilog.FilterIndex = 2
+        openFileDilog.RestoreDirectory = True
+
+        If openFileDilog.ShowDialog Then
+            FileDir = openFileDilog.FileName
+        End If
+
+        If openFileDilog.FileNames IsNot "" Then
+
+            Dim StmReader As StreamReader = New StreamReader(FileDir)
+            Dim count As Integer = 1
+            Try
+                Do While StmReader.Peek() >= 0
+                    stringReader = String.Empty
+                    stringReader = StmReader.ReadLine
+
+                    'CHECK PALINDROME FIRST LETTER:
+                    If (CheckPalindromeLetters(stringReader)) Then
+
+                        'CHECK PALINDROME WORDS AFTER REVERSING TEXT
+                        palinString = StrReverse(stringReader)
+                        If palinString.Equals(palinString) Then
+                            'CHECK PALINDROME REVERSE:
+                            If (CheckPlandromeLetterReverse(stringReader) = True) Then
+
+                                'CHECK PALINDROME WHITESPACE AND CASE SENSITIVITY
+                                If (CheckPalindromeWhiteSpaceAndCaseSentivitiy(stringReader, True, True) = True) Then
+                                    tempStr &= stringReader & "-PALINDROME" & vbNewLine
+                                End If
+
+                            End If
+                        End If
+                    Else
+                        tempStr &= stringReader & "-NOT PALINDROME" & vbNewLine
+                    End If
+                    ' Console.WriteLine(words)
+                    count += 1
+                Loop
+            Catch ex As Exception
+                MessageBox.Show(ex.ToString)
+            End Try
+            txtbox1.Text = tempStr
+
+
+        Else
+            MessageBox.Show("Please Upload correct file!!")
+            openFileDilog.Reset()
+            txtbox1.Text = ""
+        End If
+
+    End Sub
+
+    Public Function CheckPalindromeLetters(palinString As String)
+        Dim intCurr As Integer = 0
+        Dim intLength As Integer = palinString.Length - 1
+        While intCurr < intLength
+            If palinString(intCurr) <> palinString(intLength) Then
+                Return False
+            End If
+            intCurr += 1
+            intLength -= 1
+        End While
+        Return True
+    End Function
+
+    Public Function CheckPlandromeLetterReverse(strText As String)
+        Dim blnPal As Boolean = True
+
+        For i As Integer = 0 To strText.Length \ 2
+
+            If strText(i) <> strText(strText.Length - i - 1) Then
+
+                blnPal = False
+
+                Exit For
+
+            End If
+
+        Next
+
+        Return blnPal
+    End Function
+
+
+    Private Function CheckPalindromeWhiteSpaceAndCaseSentivitiy(ByVal strText As String,
+        ByVal blnWhiteSpace As Boolean,
+        ByVal blnCase As Boolean) As Boolean
+
+        Dim scCompare As StringComparison = If(blnCase,
+           StringComparison.CurrentCultureIgnoreCase,
+           StringComparison.CurrentCulture)
+
+        If blnWhiteSpace Then
+
+            Return String.Equals(strText.Replace(" ", String.Empty),
+               String.Join("", strText.Replace(" ", String.Empty) _
+               .ToArray.Reverse), scCompare)
+
+        Else
+
+            Return String.Equals(strText, String.Join("",
+               strText.ToArray.Reverse), scCompare)
+
+        End If
+    End Function
+End Class
